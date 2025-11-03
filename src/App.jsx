dea6f31f613eebf9acf67e4d03ba175ad7c0d2c6
Rikke Gleerup <rikkeglee@gmail.com>
2025-11-03 08:49:13 +0100

    branch done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header></header>
+
+      <main>
+        <article>
+          
+        </article>
+      </main>
+
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
